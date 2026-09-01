82c940fdcd5a6d2727e549c8e71e4717f8ec10d1
Alexis Kim <alexiskim121@gmail.com>
2026-09-01 08:11:23 -0400

    refactor: update Source and source identification to track all discovered formats

--- spark/ingestion.py
+++ spark/ingestion.py
@@ -1,36 +1,26 @@
 from pathlib import Path
 
 class Source:
-    def __init__(self, source_type, supported_format, location, unsupported_format):
+    def __init__(self, source_type, formats, location):
         if isinstance(source_type, str):
             self.source_type = source_type  # i.e. file or database
         else:
             raise TypeError('source type must be string.')
 
-        if isinstance(supported_format, set):
-            self.supported_format = supported_format # i.e json, postgres, parquet, etc
+        if isinstance(formats, set):
+            self.formats = formats # i.e json, postgres, parquet, etc
         else: 
             raise TypeError('format type must be set')
 
         if isinstance(location, str):
             self.location = location  # i.e filesystem path, database identifer 
         else:
-            raise TypeError('location must be string.')
-
-        if unsupported_format is not None and isinstance(unsupported_format, set):
-            self.unsupported_format = unsupported_format
-        elif unsupported_format is None:
-            self.unsupported_format = set()
-        else:
-            raise TypeError('invalid value for unsupported format')
-      
-        
+            raise TypeError('location must be string.')    
 
 
-def indentify_source(location):
+def identify_source(location):
     file_ext = set()
     format_names = {}
-    supported_formats = {'csv', 'json', 'parquet', 'avro', 'orc', 'text'}
 
     path_location = Path(location)
 
@@ -48,20 +38,15 @@ def indentify_source(location):
     for ext in file_ext:
         format_names[ext] = ext[1:].lower()
 
-    discovered_formats = set(format_names.values())
+    formats = set(format_names.values())
 
-    unsupported_format = discovered_formats - supported_formats
-
-    supported_format = discovered_formats - unsupported_format
-
-    if len(supported_format) == 0:
-        raise ValueError('no supported file formats found')
+    if len(formats) == 0:
+        raise ValueError('no file formats found')
 
     source = Source(
         'file',
-        supported_format,
+        formats,
         location,
-        unsupported_format
     )
 
     return source
