6e83a93b4f5ee989b09e206619e849ea178a9123
Alexis Kim <alexiskim121@gmail.com>
2026-09-03 17:41:48 -0400

    feat: add reading raw source abstraction

--- spark/readers/reader.py
+++ spark/readers/reader.py
@@ -7,4 +7,6 @@ class Reader(ABC):
     def read(self, source):
         """Read the source and return a Spark DataFrame."""
 
-    
+    @abstractmethod
+    def read_raw(self, file_path):
+        """Read the source and return the raw data."""
